f2e2b466ac311592071ed3079980ebd60bb2eb07
TheRhino6 <ryanandrewsmedia@gmail.com>
2026-08-11 09:18:13 +0100

    C: setting up TextNode

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+
+def main():
+    textnode = TextNode("Hello, World!", text_type="plain", url="https://www.boot.dev")
+    print (textnode)
+
+if __name__ == "__main__":
+    main()
