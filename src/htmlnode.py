41f26dc11e8a101dc9dd933743862f9bd38e270b
TheRhino6 <ryanandrewsmedia@gmail.com>
2026-08-11 16:57:06 +0100

    F: added leafnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -26,3 +26,18 @@ class HtmlNode:
             self.children == other.children and
             self.props == other.props
         )
+
+class LeafNode(HtmlNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have a value to convert to HTML")
+        if self.tag is None:
+            return f"{self.value}"
+        props_str = self.props_to_html()
+        return f"<{self.tag}{props_str}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode(tag={self.tag}, value={self.value}, props={self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,7 +1,26 @@
 import unittest
-from htmlnode import HtmlNode
+from htmlnode import HtmlNode, LeafNode
 
 class TestHtmlNode(unittest.TestCase):
+    def test_eq(self):
+        node = HtmlNode("<p>This is a paragraph</p>")
+        node2 = HtmlNode("<p>This is a paragraph</p>")
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node = HtmlNode("<p>This is a paragraph</p>")
+        node2 = HtmlNode("<div>This is a div</div>")
+        self.assertNotEqual(node, node2)
+
+    def test_eq_different_type(self):
+        node = HtmlNode("<p>This is a paragraph</p>")
+        self.assertNotEqual(node, "This is a string")
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
     def test_eq(self):
         node = HtmlNode("<p>This is a paragraph</p>")
         node2 = HtmlNode("<p>This is a paragraph</p>")
